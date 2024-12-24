267af14cae2f3fbb616e155f5e1b2a788bf53d7b
Wilson Souto <wilsonsouto@outlook.com.br>
2024-12-24 12:15:05 -0300

    Enhance main menu display with date, profit, and options for product management

--- CandyShop/Program.cs
+++ CandyShop/Program.cs
@@ -3,5 +3,16 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Mary's Candy Shop");
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("Today's date: " + DateTime.Now);
+        Console.WriteLine("Days since opening: " + 1);
+        Console.WriteLine("Today's profit: $ " + 5.5m);
+        Console.WriteLine("Today's target achieved: " + false);
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine("Choose one option: ");
+        Console.WriteLine("'V' to view product");
+        Console.WriteLine("'A' to add product");
+        Console.WriteLine("'D' to delete product");
+        Console.WriteLine("'U' to update product");
     }
 }
