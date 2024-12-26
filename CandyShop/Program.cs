2d7c794e1b56a854c603bc8a013f0b7a4ef7ce13
Wilson Souto <wilsonsouto@outlook.com.br>
2024-12-26 10:01:56 -0300

    Implement product management functionality with options to view, add, delete, and update products

--- CandyShop/Program.cs
+++ CandyShop/Program.cs
@@ -2,26 +2,85 @@
 {
     private static void Main(string[] args)
     {
+        Program program = new Program();
+
+        program.PrintHeader();
+
+        string usersChoice = Console.ReadLine().ToUpperInvariant();
+
+        switch (usersChoice)
+        {
+            case "V":
+                program.ViewProducts();
+                break;
+            case "A":
+                program.AddProduct();
+                break;
+            case "D":
+                program.DeleteProduct();
+                break;
+            case "U":
+                program.UpdateProduct();
+                break;
+            default:
+                Console.WriteLine("Invalid option");
+                break;
+        }
+    }
+
+    void ViewProducts()
+    {
+        Console.WriteLine("User chose to view the products");
+    }
+
+    void AddProduct()
+    {
+        Console.WriteLine("User chose to add the product");
+    }
+
+    void DeleteProduct()
+    {
+        Console.WriteLine("User chose to delete the product");
+    }
+
+    void UpdateProduct()
+    {
+        Console.WriteLine("User chose to update the product");
+    }
+
+    void PrintHeader()
+    {
         string shopName = "Mary's Candy Shop";
         string separator = "--------------------------------------------------";
         DateTime currentDate = DateTime.Now;
-        int daysSinceOpening = 1;
         decimal todaysProfit = 5.5m;
         bool targetAchieved = false;
-        string menu =
+
+        Console.WriteLine($"{shopName}\n" +
+        $"{separator}\n" +
+        $"Today's date: {currentDate:dd/MM/yyyy}\n" +
+        $"Days since opening: {GetDaysSinceOpening()}\n" +
+        $"Today's profit: $ {todaysProfit}\n" +
+        $"Today's target achieved: {targetAchieved}\n" +
+        $"{separator}\n" +
+        $"{GetMenu()}");
+    }
+
+    string GetMenu()
+    {
+        return
         "Choose one option:\n" +
         "'V' to view products\n" +
         "'A' to add product\n" +
         "'D' to delete product\n" +
         "'U' to update product\n";
+    }
 
-        Console.WriteLine(shopName);
-        Console.WriteLine(separator);
-        Console.WriteLine("Today's date: " + currentDate);
-        Console.WriteLine("Days since opening: " + daysSinceOpening);
-        Console.WriteLine("Today's profit: $ " + todaysProfit);
-        Console.WriteLine("Today's target achieved: " + targetAchieved);
-        Console.WriteLine(separator);
-        Console.WriteLine(menu);
+    int GetDaysSinceOpening()
+    {
+        DateTime openingDate = new DateTime(1997, 1, 25);
+        TimeSpan timeDifference = DateTime.Now - openingDate;
+
+        return timeDifference.Days;
     }
 }
