6bdb6850e44028e05191d6b05c0ef99aaa6042dc
Wilson Souto <wilsonsouto@outlook.com.br>
2024-12-27 00:23:26 -0300

    Refactor Product model: encapsulate Name property with validation and capitalization

--- CandyShop/Models/Product.cs
+++ CandyShop/Models/Product.cs
@@ -1,16 +1,36 @@
+
 namespace CandyShop.Models
 {
     internal class Product
     {
-        internal string Name { get; set; }
+        private string _name { get; set; } = "";
 
-        internal int Id { get; }
+        internal string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Name must be at least 3 characters long.");
+                }
+
+                var result = Helpers.CapitalizeFirstLetter(value);
+                _name = result;
+            }
+        }
+
+        internal int Id { get; set; }
 
         internal decimal Price { get; set; }
-
-        public Product(int id)
-        {
-            Id = id;
-        }
     }
 }
