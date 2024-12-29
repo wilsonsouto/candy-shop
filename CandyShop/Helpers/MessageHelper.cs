16d80a1912a4463bb381577965b063251a7731d3
Wilson Souto <wilsonsouto@outlook.com.br>
2024-12-29 11:24:30 -0300

    Add constant messages for product input validation in MessageHelper class

---
+++ CandyShop/Helpers/MessageHelper.cs
@@ -0,0 +1,19 @@
+namespace CandyShop.Helpers
+{
+    internal static class MessageHelper
+    {
+        public const string Name = "Enter the product name: ";
+
+        public const string InvalidName =
+            "The product name cannot be empty and must be at least 3 characters long. Try again: ";
+
+        public const string Price = "Enter the product price: ";
+
+        public const string Cocoa = "Enter the cocoa percentage: ";
+
+        public const string Shape = "Enter the shape of the lollipop: ";
+
+        public const string InvalidShape =
+            "The shape cannot be empty and must be at least 3 characters long. Try again: ";
+    }
+}
